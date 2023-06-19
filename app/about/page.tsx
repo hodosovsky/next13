13d322eee68b08bad9ff9ab11f4b4ea34047289b
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-06-19 11:50:29 +0300

    first comit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About us",
+};
+
+export default function About() {
+  return <h2>Select subitem</h2>;
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="container">
+      &copy;{new Date().getFullYear()} all rights reserved
+    </footer>
+  );
+};
+
+export { Footer };
